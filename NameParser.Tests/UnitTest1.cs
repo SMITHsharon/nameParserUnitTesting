0c39935c8f63ba7bfaed884c61f9ce86e4037e2d
sharon smith <sharon.e.smith2@gmail.com>
2017-07-26 22:47:05 -0500

    commit working code for required test names

--- NameParser.Tests/UnitTest1.cs
+++ NameParser.Tests/UnitTest1.cs
@@ -20,6 +20,7 @@ namespace NameParser.Tests
             Assert.Equal(result.FirstName, "Zandaya");
         }
 
+
         [Theory]
         [InlineData("Steve Jones", "Steve", "Jones")]
         [InlineData("Mary Barnes-Jones", "Mary", "Barnes-Jones")]
@@ -39,6 +40,7 @@ namespace NameParser.Tests
             Assert.Equal(result.LastName, expectedLastName);
         }
 
+
         [Theory]
         [InlineData("Steve L. Jones", "Steve", "L.", "Jones")]
         [InlineData("Steve X Jones", "Steve", "X", "Jones")]
@@ -47,7 +49,7 @@ namespace NameParser.Tests
                                                                   string expectedMiddleInitial, string expectedLastName)
         {
             //Arrange
-            // var name = "Steve Jones";
+            // var name = "Steve X Jones";
             var nameParser = new Parser();
 
             //Act
@@ -56,6 +58,48 @@ namespace NameParser.Tests
             //Assert
             Assert.Equal(result.FirstName, expectedFirstName);
             Assert.Equal(result.MiddleInitial, expectedMiddleInitial);
+            Assert.Equal(result.LastName, expectedLastName);
+        }
+
+
+        [Theory]
+        [InlineData("Bob Marley Jr.", "Bob", "Marley", "Jr.")]
+        [InlineData("Bob Marley Esq", "Bob", "Marley", "Esq")]
+        // [Fact]
+        public void FirstAndLastNameArePopulatedPlusSuffix(string nameToTest, string expectedFirstName, 
+                                                           string expectedLastName, string expectedSuffix)
+        {
+            //Arrange
+            // var name = "Bob Marley Jr.";
+            var nameParser = new Parser();
+
+            //Act
+            var result = nameParser.ParseName(nameToTest);
+
+            //Assert
+            Assert.Equal(result.FirstName, expectedFirstName);
+            Assert.Equal(result.LastName, expectedLastName);
+            Assert.Equal(result.Suffix, expectedSuffix);
+        }
+
+
+        [Theory]
+        [InlineData("Mr. Bob Michaels", "Mr.", "Bob", "Michaels")]
+        [InlineData("Mrs. Aunt Jemima", "Mrs.", "Aunt", "Jemima")]
+        // [Fact]
+        public void PrefixPlusFirstAndLastNamePopulated(string nameToTest, string expectedPrefix, 
+                                                           string expectedFirstName, string expectedLastName)
+        {
+            //Arrange
+            // var name = "Mr. Bob Michaels";
+            var nameParser = new Parser();
+
+            //Act
+            var result = nameParser.ParseName(nameToTest);
+
+            //Assert
+            Assert.Equal(result.Prefix, expectedPrefix);
+            Assert.Equal(result.FirstName, expectedFirstName);
             Assert.Equal(result.LastName, expectedLastName);
         }
     }
--- NameParser/Parser.cs
+++ NameParser/Parser.cs
@@ -9,17 +9,39 @@ namespace NameParser
             var user = new User();
 
             var namePieces = name.Split(' ');
+            var index = 0;
 
-            user.FirstName = namePieces[0];
-            if (namePieces.Length > 1)
+            // check for Prefix
+            var prefix = false;
+            char last = namePieces[0][namePieces[0].Length - 1];
+            if (last == '.')
             {
-                if (namePieces[1].Length == 1 || namePieces[1][1] == '.')
+                user.Prefix = namePieces[index];
+                prefix = true;
+                index++;
+            }
+            
+            // First Name
+            user.FirstName = namePieces[index];
+            index++;
+
+            // Middle Initial / Last Name / Suffix
+            if (namePieces.Length > 1) 
+            {
+                if (namePieces[index].Length == 1 || namePieces[index][1] == '.')
                 {
-                    user.MiddleInitial = namePieces[1];
+                    user.MiddleInitial = namePieces[index];
+                    user.LastName = namePieces[index+1];
+                    index += 2;
                 }
                 else
                 {
-                    user.LastName = namePieces[1];
+                    user.LastName = namePieces[index];
+                    index++;
+                    if (namePieces.Length > 2 && !prefix) 
+                    {
+                        user.Suffix = namePieces[index];
+                    }
                 }
             }
 
